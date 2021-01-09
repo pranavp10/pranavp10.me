6509cf42a14b40d712561e16e8ebf8410907e12f
Pranav Patel <pranav@quze.co>
2021-01-09 08:24:43 +0530

    dark switch added

--- pages/_app.js
+++ pages/_app.js
@@ -2,11 +2,15 @@
 import '../styles/index.css';
 import PropTypes from 'prop-types';
 import { ThemeProvider } from 'next-themes';
+import Head from 'next/head';
 import Nav from '../components/nav';
 
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider attribute="class">
+      <Head>
+        <meta content="width=device-width, initial-scale=1" name="viewport" />
+      </Head>
       <div className="flex flex-col min-h-screen overflow-hidden">
         <Nav />
         <Component {...pageProps} />
