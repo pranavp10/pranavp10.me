40b477f400ed7aae1788aeac594effc771b8d8dd
Pranav Patel <pranav@quze.co>
2021-02-11 12:10:13 +0530

    :mag: global SEO added

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,17 @@
 import '@/styles/nprogress.css';
 import '@/styles/globals.css';
 import PropTypes from 'prop-types';
+import { ThemeProvider } from 'next-themes';
 import { DefaultSeo } from 'next-seo';
+import DefaultSEO from '@/utils/next-seo.config';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider attribute="class">
+      <DefaultSeo {...DefaultSEO} />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
 MyApp.propTypes = {
