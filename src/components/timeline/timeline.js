74db8735f05d158f257c6f382587ead54a49f78f
Pranav Patel <pranav@quze.co>
2021-02-12 21:56:55 +0530

    :memo: updated time line

--- src/components/timeline/timeline.js
+++ src/components/timeline/timeline.js
@@ -1,5 +1,8 @@
-import { FaLaptopCode, FaGraduationCap, FaMobileAlt, FaCode } from 'react-icons/fa';
+import { FaLaptopCode, FaGraduationCap, FaMobileAlt, FaBoxOpen } from 'react-icons/fa';
+import { AiOutlineCode, AiOutlineGlobal } from 'react-icons/ai';
 import { IoNewspaper } from 'react-icons/io5';
+import { SiVisualstudiocode } from 'react-icons/si';
+import { BiBuildings } from 'react-icons/bi';
 import DateEntry from './dateEntry';
 import TimeLineEntry from './timelineEntry';
 
@@ -9,8 +12,45 @@ export default function Timeline() {
       <h3 className="font-bold text-2xl md:text-4xl tracking-tight mb-4 mt-8 text-black dark:text-white">
         Timeline
       </h3>
+      <DateEntry title="2021" />
+      <TimeLineEntry title="Personal Website" Icon={AiOutlineGlobal} tint="indigo">
+        <p className="text-gray-700 dark:text-gray-400">
+          Launching person website which is built using tailwind css and next js
+        </p>
+      </TimeLineEntry>
+
+      <DateEntry title="2020" />
+      <TimeLineEntry title="Frontend Libraries" Icon={FaBoxOpen} tint="red">
+        <p className="text-gray-700 dark:text-gray-400">
+          Best{' '}
+          <a target="_black" rel="noopener" className="underline" href="https://libraries.now.sh/">
+            Frontend Libraries
+          </a>{' '}
+          which are used by professionals working at facebook, Google, Microsoft and many other top
+          companies.
+        </p>
+      </TimeLineEntry>
+      <TimeLineEntry title="My Code editor" Icon={SiVisualstudiocode} tint="blue">
+        <p className="text-gray-700 dark:text-gray-400">
+          A code editor for Frontend development that improves your efficiency.
+        </p>
+      </TimeLineEntry>
+      <TimeLineEntry title="Dev Env" Icon={AiOutlineCode} tint="gray">
+        <p className="text-gray-700 dark:text-gray-400">
+          Developed the first website which will help the developers to setup the{' '}
+          <a target="_black" rel="noopener" className="underline" href="https://devenv.now.sh/">
+            dev environment
+          </a>
+          .
+        </p>
+      </TimeLineEntry>
+      <TimeLineEntry title="Joined Quze Inc" Icon={BiBuildings} tint="yellow">
+        <p className="text-gray-700 dark:text-gray-400">
+          I was really exited to join as Frontend developer. The learning phase of the start up
+        </p>
+      </TimeLineEntry>
       <DateEntry title="2019" />
-      <TimeLineEntry title="Joined Tulasi Technologies" Icon={FaCode} tint="blue">
+      <TimeLineEntry title="Joined Tulasi Technologies" Icon={BiBuildings} tint="yellow">
         <p className="text-gray-700 dark:text-gray-400">
           Worked as a software Engineer and learnt lots of things during this phase
         </p>
