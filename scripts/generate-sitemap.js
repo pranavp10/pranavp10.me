a72fc6a5ac75c3710fd7fc29909864eb0909ed2f
Pranav Patel <pranav@quze.co>
2021-02-12 17:05:35 +0530

    :hammer: added sitemap script and next js config file

---
+++ scripts/generate-sitemap.js
@@ -0,0 +1,41 @@
+/* eslint-disable import/no-extraneous-dependencies */
+const fs = require('fs');
+
+const globby = require('globby');
+const prettier = require('prettier');
+
+(async () => {
+  const prettierConfig = await prettier.resolveConfig('./.prettierrc.js');
+  const pages = await globby(['src/pages/**/*{.js,.mdx}', '!src/pages/_*.js', '!src/pages/api']);
+
+  const sitemap = `
+        <?xml version="1.0" encoding="UTF-8"?>
+        <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+            ${pages
+              .map((page) => {
+                const path = page
+                  .replace('src', '')
+                  .replace('pages', '')
+                  .replace('data', '')
+                  .replace('.js', '')
+                  .replace('.mdx', '')
+                  .replace('//', '/');
+                const route = path === '/index' ? '' : path;
+                return `
+                        <url>
+                            <loc>${`https://pranavp10.now.sh${route}`}</loc>
+                        </url>
+                    `;
+              })
+              .join('')}
+        </urlset>
+    `;
+
+  const formatted = prettier.format(sitemap, {
+    ...prettierConfig,
+    parser: 'html',
+  });
+
+  // eslint-disable-next-line no-sync
+  fs.writeFileSync('public/sitemap.xml', formatted);
+})();
