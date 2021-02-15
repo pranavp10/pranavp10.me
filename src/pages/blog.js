257095f39b94694e7d0ca0afbc2b31091fbd082a
Pranav Patel <pranav@quze.co>
2021-02-15 11:06:02 +0530

    :pencil2: typo fixed

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,21 +1,15 @@
 import PropTypes from 'prop-types';
-import { useState } from 'react';
-import Container from '@/components/blog/layout';
 import BlogPost from '@/components/blog/blogPost';
 import { getAllFilesFrontMatter } from '@/components/blog/mdx';
 
 const Blog = ({ posts }) => {
-  const [searchValue, setSearchValue] = useState('');
-  const filteredBlogPosts = posts
-    .sort((a, b) => Number(new Date(b.publishedAt)) - Number(new Date(a.publishedAt)))
-    .filter((frontMatter) => frontMatter.title.toLowerCase().includes(searchValue.toLowerCase()));
-
+  const filteredBlogPosts = posts.sort(
+    (a, b) => Number(new Date(b.publishedAt)) - Number(new Date(a.publishedAt))
+  );
   return (
     <div className="pt-32">
       <div className="flex flex-col justify-center items-start max-w-2xl mx-auto mb-16">
-        <h1 className="font-bold text-3xl md:text-5xl tracking-tight mb-4 text-black dark:text-white">
-          Blog
-        </h1>
+        <h1 className="text-xl font-bold text-gray-700 md:text-2xl">Post</h1>
         {filteredBlogPosts.map((frontMatter) => (
           <BlogPost key={frontMatter.title} {...frontMatter} />
         ))}
