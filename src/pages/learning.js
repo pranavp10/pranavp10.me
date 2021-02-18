fd09523c636106b3d7149b62aea777d11e4fe09d
Pranav Patel <pranav@quze.co>
2021-02-18 23:14:01 +0530

    learnig page created

---
+++ src/pages/learning.js
@@ -0,0 +1,9 @@
+const Learning = () => (
+  <div className="pt-32">
+    <div className="flex flex-col justify-center items-start max-w-2xl mx-auto mb-16">
+      <h1 className="text-xl font-bold text-gray-700 md:text-2xl">Learning Coming soon</h1>
+    </div>
+  </div>
+);
+
+export default Learning;
