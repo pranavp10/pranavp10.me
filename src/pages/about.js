5cd2f9a82c3554632c86fa3b348d9f3dcc278901
Pranav Patel <pranav@quze.co>
2021-03-01 16:31:51 +0530

    :memo: about page updated

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,19 +1,59 @@
+import Link from 'next/link';
 import { BsCheckCircle } from 'react-icons/bs';
 
 const About = () => (
   <div className="pt-32">
-    <h1
-      className="text-5xl font-extrabold leading-tighter tracking-tighter mb-4 text-center"
-      data-aos="zoom-y-out">
-      Hello! I'm Pranav,
+    <h1 className="text-5xl font-extrabold leading-tighter tracking-tighter mb-4 text-center">
+      👋 I'm Pranav,
     </h1>
     <div className="space-y-4">
-      <div className="flex justify-center items-center">
+      <div className="flex items-center">
         <div className="text-green-500 p-4">
           <BsCheckCircle />
         </div>
-        <p className="text-gray-600 dark:text-gray-100" data-aos="zoom-y-out" data-aos-delay="150">
-          A software engineer by passion but love solving problems and enjoy working on JavaScript,
+        <p className="text-gray-600 dark:text-gray-100">
+          I'm a frontend developer at <b>Q</b>
+          uze which is going to effect millions of students
+        </p>
+      </div>
+      <div className="flex items-center">
+        <div className="text-green-500 p-4">
+          <BsCheckCircle />
+        </div>
+        <p className="text-gray-600 dark:text-gray-100">
+          Current Technology I am working on{' '}
+          <a
+            href="https://nextjs.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-blue-500">
+            Next js (The React Framework)
+          </a>{' '}
+          and{' '}
+          <a
+            href="https://tailwindcss.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-blue-500">
+            Tailwind css
+          </a>{' '}
+          and check out the list of{' '}
+          <a
+            href="https://libraries.pranavp10.me/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-blue-500">
+            Framework and Libraries
+          </a>{' '}
+          I have used till now.
+        </p>
+      </div>
+      <div className="flex items-center">
+        <div className="text-green-500 p-4">
+          <BsCheckCircle />
+        </div>
+        <p className="text-gray-600 dark:text-gray-100">
+          A software engineer by passion, love solving problems and enjoy working on JavaScript,
           React and Design Systems(learning).
         </p>
       </div>
@@ -21,7 +61,7 @@ const About = () => (
         <div className="text-green-500 p-4">
           <BsCheckCircle />
         </div>
-        <p className="text-gray-600 dark:text-gray-100" data-aos="zoom-y-out" data-aos-delay="150">
+        <p className="text-gray-600 dark:text-gray-100">
           Contribute to the open source project like{' '}
           <a
             href="https://github.com/searchkit/searchkit"
@@ -39,6 +79,18 @@ const About = () => (
             @LoginRadius
           </a>{' '}
           and planing to contribute to more open source project in the feature
+        </p>
+      </div>
+      <div className="flex items-center">
+        <div className="text-green-500 p-4">
+          <BsCheckCircle />
+        </div>
+        <p className="text-gray-600 dark:text-gray-100">
+          I always right down all my{' '}
+          <Link href="/learning">
+            <a className="text-blue-500">learning Process</a>
+          </Link>{' '}
+          and share with the people so that it will help others to learn what I have learnt.
         </p>
       </div>
     </div>
