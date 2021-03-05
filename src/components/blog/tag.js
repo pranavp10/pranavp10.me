45442d27b29186bfeb0cf3233d5c2fa513e240d3
Pranav Patel <pranav@quze.co>
2021-03-05 15:17:01 +0530

    :lipstick: ui updated

--- src/components/blog/tag.js
+++ src/components/blog/tag.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 
 const Tag = ({ text }) => (
   <Link href="/tags">
-    <a className="mr-3 text-sm font-medium text-blue-500 uppercase hover:text-blue-600 dark:hover:text-blue-400">
+    <a className="mr-3 text-sm font-medium text-gray-500 uppercase hover:text-gray-600 dark:hover:text-gray-400">
       {text.split(' ').join('-')}
     </a>
   </Link>
--- src/pages/learning.js
+++ src/pages/learning.js
@@ -1,20 +1,46 @@
 import PropTypes from 'prop-types';
 import BlogPost from '@/components/blog/blogPost';
 import { getAllFilesFrontMatter } from '@/components/blog/mdx';
+import { useState } from 'react';
 
 const Blog = ({ posts }) => {
-  const filteredBlogPosts = posts.sort(
-    (a, b) => Number(new Date(b.publishedAt)) - Number(new Date(a.publishedAt))
-  );
+  const [searchValue, setSearchValue] = useState('');
+  const filteredBlogPosts = posts.filter((frontMatter) => {
+    const searchContent = frontMatter.title + frontMatter.summary + frontMatter.tags.join(' ');
+    return searchContent.toLowerCase().includes(searchValue.toLowerCase());
+  });
+
   return (
     <div className="pt-32">
       <div className="flex flex-col justify-center items-start max-w-2xl mx-auto mb-16">
         <div className="max-w-3xl pb-8 md:pb-16 text-center md:text-left">
           <h1 className="text-5xl font-black">My Learning Book</h1>
-          <p className="text-gray-600 dark:text-gray-100 mt-4">
+          <p className="text-gray-600 dark:text-gray-100 my-4">
             Welcome to my learning book. You will find all my learning documentation here, revise
             every thing and started understanding
           </p>
+          <div className="relative w-full">
+            <input
+              aria-label="Search Learning Resource"
+              type="text"
+              onChange={(e) => setSearchValue(e.target.value)}
+              placeholder="Search Learning Resource"
+              className="block w-full px-4 py-2 text-gray-900 bg-white border border-gray-300 rounded-md dark:border-gray-900 focus:ring-gray-500 focus:border-gray-500 dark:bg-gray-800 dark:text-gray-100"
+            />
+            <svg
+              className="absolute w-5 h-5 text-gray-400 right-3 top-3 dark:text-gray-300"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor">
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+              />
+            </svg>
+          </div>
         </div>
         {filteredBlogPosts.map((frontMatter) => (
           <BlogPost
